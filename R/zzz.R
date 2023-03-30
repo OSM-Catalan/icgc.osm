9fae103195518b3e0bb85e949b0074912934cac8
Joan Maspons <joanmaspons@gmail.com>
2023-03-30 22:22:02 +0200

    Correccions menors i increment de versió

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   msg <- paste0(
-    "Dades sota llicència ODbL 1.0. (c) Col·laboradors d'OpenStreetMap ",
-    " https://www.openstreetmap.org/copyright i ",
-    "ICGC https://wiki.openstreetmap.org/wiki/File:20230109_CartaObertaICGC-OSM_SIGNADA.pdf"
+    "Dades sota llic\u00E8ncia ODbL 1.0. (c) Col\u00B7laboradors d'OpenStreetMap ",
+    "https://www.openstreetmap.org/copyright i ICGC ",
+    "https://wiki.openstreetmap.org/wiki/File:20230109_CartaObertaICGC-OSM_SIGNADA.pdf"
   )
   packageStartupMessage(msg)
 }
